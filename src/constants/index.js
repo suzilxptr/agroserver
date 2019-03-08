0a86f75570d25fd215cbd5746a4d6901f4b3f9b3
suzilxptr <sushil.bastola@movial.com>
2019-03-08 09:48:11 +0200

    agrostore separated server

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const isDev = process.env.NODE_ENV === 'development';
+
+export const DB_URL = 'mongodb://sachin1:sachin1@ds123465.mlab.com:23465/agrostore' || 'mongodb://localhost:27017/agrostore-dev';
