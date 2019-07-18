b52586b08e06db9bba0a585781c8d8bb22641e16
Sushil <suzilxptr@gmail.com>
2019-07-18 19:53:23 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const DB_URL = 'mongodb://sachin1:sachin1@ds123465.mlab.com:23465/agrostore' || 'mongodb://localhost:27017/agrostore-dev';
+export const DB_URL = 'mongodb://<username>:<password>@ds123465.mlab.com:<port>/agrostore' || 'mongodb://localhost:27017/agrostore-dev';
